758afbf871080185e2e0e3300d0eeec60e95624f
Angel <angel@Angels-MacBook-Air.local>
2023-01-18 18:20:37 -0500

    completed

--- index.js
+++ index.js
@@ -1,25 +1,52 @@
 /**
  * Adds a new store to the very end of the list.
- * @param {Object[]]} stores - An array of store objects.
+ * @param {Object[]} stores - An array of store objects.
  * @param {Object} store - An object representing a single store. See the instructions for details on its shape.
  * @returns {Object[]} The same `stores` array that was inputted.
  */
-function addNewStore(stores, store) {}
+function addNewStore(stores, store) {
+  // find an array of store in object
+  // store is representing a single store in an object
+  stores.push(store)
+
+  // return back to the orignial object
+  return stores
+}
+  
 
 /**
  * Removes a store object at the given position.
- * @param {Object[]]} stores - An array of store objects.
+ * @param {Object[]} stores - An array of store objects.
  * @param {number} index - A number representing the index of the store to be removed from the array.
  * @returns {Object[]} The same `stores` array that was inputted.
  */
-function removeStoreAtPosition(stores, index) {}
+function removeStoreAtPosition(stores, index) {
+  // check the array of stores in object
+  // check the index that is representing the index of the store to be removed from the array
 
+
+  // use the splice method to remove the index of the store
+  stores.splice(index, 1)
+  
+  return stores
+
+
+}
 /**
  * Creates a duplicate of the `store` object. No references should be shared between the inputted `store` and the result.
  * @param {Object} store - An object representing a single store. See the instructions for details on its shape.
  * @returns {Object} The duplicated store object. This should not be the same as the store that was inputted.
  */
-function duplicateStore(store) {}
+function duplicateStore(store) {
+  // Check an object that is representing a single store
+  // how do you duplicate the store object?
+  // return an object that is not the same as the imput.
+
+  // How do you know which duplicate method works??
+  let copiedStore = JSON.parse(JSON.stringify(store))
+
+  return copiedStore
+}
 
 module.exports = {
   addNewStore,
