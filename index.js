d420861af7a402c286c803957750e35f9ae2612b
Mussie <mussierussom@pursuit.org>
2023-01-19 09:50:38 -0500

    It runs well except some of secdond question. I'm still working on it.

--- index.js
+++ index.js
@@ -4,7 +4,17 @@
  * @param {Object} store - An object representing a single store. See the instructions for details on its shape.
  * @returns {Object[]} The same `stores` array that was inputted.
  */
-function addNewStore(stores, store) {}
+function addNewStore(stores, store) {
+  stores.push(store);
+return stores;
+  // test("it should modify the `stores` array by adding a new store", () => {
+  //   const stores = [];
+  //   const newStore = {
+  //     street: "42-11 Broadway",
+  //     city: "Astoria",
+  //     state: "NY",
+  //     zip: "11103",
+}
 
 /**
  * Removes a store object at the given position.
@@ -12,14 +22,47 @@ function addNewStore(stores, store) {}
  * @param {number} index - A number representing the index of the store to be removed from the array.
  * @returns {Object[]} The same `stores` array that was inputted.
  */
-function removeStoreAtPosition(stores, index) {}
+function removeStoreAtPosition(stores, index) {
+  let newStores = [];
+  newStores = stores.slice([index], 1);
+  newStores = stores;
+  newStores = stores.pop();
+  newStores = stores.slice([2], 1);
+
+  return stores;
+}
 
 /**
  * Creates a duplicate of the `store` object. No references should be shared between the inputted `store` and the result.
  * @param {Object} store - An object representing a single store. See the instructions for details on its shape.
  * @returns {Object} The duplicated store object. This should not be the same as the store that was inputted.
  */
-function duplicateStore(store) {}
+function duplicateStore(store) {
+
+  let duplicStore = Object.assign({}, store);
+
+  duplicStore.boardGames = Object.assign([], store.boardGames);
+  duplicStore.address = Object.assign({}, store.address);
+ 
+  // test("it should use a new array for the `boardGames` key", () => {
+  //   const store = {
+  //     name: "Gamestoria",
+  //     boardGames: [
+  //       "Terra Mystica",
+  //       "Alhambra",
+  //       "Scythe",
+  //       "Carcassonne",
+  //       "Azul",
+  //     ],
+  //     address: {
+  //       street: "42-11 Broadway",
+  //       city: "Astoria",
+  //       state: "NY",
+  //       zip: "11103",
+  //     },
+
+  return duplicStore;
+}
 
 module.exports = {
   addNewStore,
