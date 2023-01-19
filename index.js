a408065e60080a19290a690d79d63fd76fc1c8b4
Rubby <rubenslombard@pursuit.org>
2023-01-19 10:03:57 -0500

    adding changes and modified index.js

--- index.js
+++ index.js
@@ -4,7 +4,26 @@
  * @param {Object} store - An object representing a single store. See the instructions for details on its shape.
  * @returns {Object[]} The same `stores` array that was inputted.
  */
-function addNewStore(stores, store) {}
+function addNewStore(stores, store) {
+
+  const Array = [ 
+        {
+         "city": "Astoria",
+         "state": "NY",
+         "street": "42-11 Broadway",
+         "zip": "11103",
+       },
+     ];
+
+  const newStore = Array;
+ store.push();
+
+  //newStore.concat(store)
+
+console.log(newStore)
+
+return stores;
+}
 
 /**
  * Removes a store object at the given position.
@@ -12,14 +31,28 @@ function addNewStore(stores, store) {}
  * @param {number} index - A number representing the index of the store to be removed from the array.
  * @returns {Object[]} The same `stores` array that was inputted.
  */
-function removeStoreAtPosition(stores, index) {}
+function removeStoreAtPosition(stores, index) {
+
+let removeStoreAtPosition = stores
+
+removeStoreAtPosition.pop()
+  return store
+}
 
 /**
  * Creates a duplicate of the `store` object. No references should be shared between the inputted `store` and the result.
  * @param {Object} store - An object representing a single store. See the instructions for details on its shape.
  * @returns {Object} The duplicated store object. This should not be the same as the store that was inputted.
  */
-function duplicateStore(store) {}
+function duplicateStore(store) {
+
+
+let storeDuplicate = Object.assign({}, store);
+//toreDuplicate.games
+
+
+  return store
+}
 
 module.exports = {
   addNewStore,
